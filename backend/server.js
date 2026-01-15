a2113d3425814bdbc124132107087200caeb0935
Deepak Singh Rawat <deepaksinghrawat141@gmail.com>
2026-01-15 15:00:21 +0530

    Update server to listen on '0.0.0.0' address
    Fix server listening address to use '0.0.0.0'.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || '0.0.0.0';
 const MONGO = process.env.MONGO_URI;
 
 // ✅ Connect first, then start server
@@ -35,7 +35,7 @@ connectDB(MONGO)
       res.status(500).json({ error: "Server error" });
     });
 
-    app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+    app.listen(PORT,'0.0.0.0' () => console.log(` Server running on port ${PORT}`));
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB:", err.message);
